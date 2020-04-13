266c93e7db2007a43d930dcb3cc787ae3f5a1791
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-13 12:07:36 +0530

    import file system, create a new file & write synchronously using fs.writeFileSync

---
+++ first-app.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+
+// To create a new file
+fs.writeFileSync('repl.txt', 'REPL(Read Evaluate Print Loop) - This process executes when we just type node in terminal(Read), instructions get executed(Evaluate), logs result into terminal(Print), and Node.js runtime waiting for new instructions to execute(Loop).');
