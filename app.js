3f40cc8b06946b61801b7be767bca6c5aa6f8980
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-18 19:27:45 +0530

    Splitting routes and create server files Split the code into routes.js and app.js and managing connectivity using exports and require.

--- app.js
+++ app.js
@@ -2,51 +2,13 @@
 
 // create a node server
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {   // createServer returns a server object which is stored in variable.
-    
-    const url = req.url;
-    const method = req.method;
+const routes = require('./routes');
 
-        if(url === '/') {
-            res.setHeader('Content-Type', 'text/html');
-            res.write('<html>');
-            res.write('<head><title>Enter Message</title></head>');
-            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-            res.write('<html>');
-            return res.end();
-        }
+console.log(routes.someText);
 
-        if(url === '/message' && method === 'POST') {
-            const body = [];
-
-            req.on('data', (chunk) => {
-                body.push(chunk);
-                console.log(chunk);
-            });
-
-            return req.on('end', () => {
-                const parsedBody = Buffer.concat(body).toString();
-                console.log(parsedBody);
-                const message = parsedBody.split('=')[1];
-               
-                fs.writeFile('message.txt', message, (err) => {
-                    res.statusCode = 302;              //procedure to redirect to a page
-                    res.setHeader('Location', '/');
-                   return res.end();
-                });  // writeFileSync(filename, message) is writing to file synchronously which will block the thread, hence writeFile(filename, message, callback) to make this process asynchronously.
-            })
-           
-        }
-
-            res.setHeader('Content-Type', 'text/html');
-            res.write('<html>');
-            res.write('<head><title>Response</title></head>');
-            res.write('<body><h1>Hello from node.js server</h1></body>');
-            res.write('<html>');
-            res.end();
-
-        });
+const server = http.createServer(routes.handler);   // createServer returns a server object which is stored in variable.
 
 server.listen(3000);       // listen allows nodejs to receive incoming requests instead of quitting. listen takes two optional arguments(port, hostname=localhost in development)
+
+
