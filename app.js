f00b6668dfa730bdcc9861061217bdedda4ad676
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-19 02:14:04 +0530

    Middlewares and use() in express  middlewares - set of functions(request handlers) through which requests are funneled until a response is sent from one of the middleware.  Helps to split the business logic into modules(middlewares) to handle requests.  Even, 3rd party packages provide such middleware functions which can be plugged into express middlewares(flexible).  next() is a function passed as an argument to the function by express to funnel requests to next middlewares.  // use() allows to use a new middleware function. use() accepts an array of request handlers.  next();    // allows the request to continue to next middleware in line. We must return a response in a middleware if next() is not used otherwise the request dies.

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const http = require('http');
 
-const express = require('express');   // express exports a express() function to create an instance
+const express = require('express');   
 
-const app = express();   // creating an express app & storing in variable. Now, the app is a valid request handler.
+const app = express();   
 
-const server = http.createServer(app);    // app - requesthandler  
+app.use((req, res, next) => {   // use() allows to use a new middleware function. use() accepts an array of request handlers
+    console.log('in the middleware');
+    next();    // allows the request to continue to next middleware in line. We must return a response in a middleware if next() is not used otherwise the request dies.
+});     
 
-server.listen(3000);       
+app.use((req, res, next) => {   // use() allows to use a new middleware function. use() accepts an array of request handlers
+    console.log('in the next middleware');
+});
 
+const server = http.createServer(app);   
+
+server.listen(3000); 
+
+// middlewares - set of functions(request handlers) through which requests are funneled until a response is sent from one of the middleware.
+// Helps to split the business logic into modules(middlewares) to handle requests.
+// Even, 3rd party packages provide such middleware functions which can be plugged into express middlewares(flexible). 
+// next() is a function passed as an argument to the function by express to funnel requests to next middlewares.
 
