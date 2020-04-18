0a75b926f46881bbe51a9a5e28ec2957563701a4
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-19 02:27:21 +0530

    sending response from middleware send() allows to send a response. express by default sets the headers using send().

--- app.js
+++ app.js
@@ -4,21 +4,19 @@ const express = require('express');
 
 const app = express();   
 
-app.use((req, res, next) => {   // use() allows to use a new middleware function. use() accepts an array of request handlers
+app.use((req, res, next) => {   
     console.log('in the middleware');
-    next();    // allows the request to continue to next middleware in line. We must return a response in a middleware if next() is not used otherwise the request dies.
+    next();    
 });     
 
-app.use((req, res, next) => {   // use() allows to use a new middleware function. use() accepts an array of request handlers
+app.use((req, res, next) => {                     // send response from this middleware.
     console.log('in the next middleware');
+    res.send('<h1>Hello from express</h1>');             // send() allows to send a response.
 });
 
 const server = http.createServer(app);   
 
 server.listen(3000); 
 
-// middlewares - set of functions(request handlers) through which requests are funneled until a response is sent from one of the middleware.
-// Helps to split the business logic into modules(middlewares) to handle requests.
-// Even, 3rd party packages provide such middleware functions which can be plugged into express middlewares(flexible). 
-// next() is a function passed as an argument to the function by express to funnel requests to next middlewares.
+
 
