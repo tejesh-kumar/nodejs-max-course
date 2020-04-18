8ca354ca28c6a5520eb5cdab94c7c16e9618bdf3
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-19 02:37:20 +0530

    shortcut to createServer by express.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');   
 
@@ -14,9 +14,10 @@ app.use((req, res, next) => {                     // send response from this mid
     res.send('<h1>Hello from express</h1>');             // send() allows to send a response.
 });
 
-const server = http.createServer(app);   
+// const server = http.createServer(app);   
 
-server.listen(3000); 
+// server.listen(3000); 
 
+app.listen(3000);    // shortcut provided by express equivalent to spin up a server and listen to requests.
 
 
