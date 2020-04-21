220fb0b6a84e62a32233549a8e5d40e676168605
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-22 00:57:23 +0530

    Creating importing common layout into all pug file  block styles - block keyword in pug allows to add extra html lines in other files (eg:- additional css files linked with link tags for which 'styles' is placeholder). extends layouts/main-layout.pug   // extends the common layout into other pages(eg:- header). block content            // defining what the content variable must render in 404 page.     h1 Page not found

--- app.js
+++ app.js
@@ -21,23 +21,15 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {docTitle: '404 page'});
+    res.status(404).render('404', {pageTitle: 'Page not found'});
 })
 
 app.listen(3000);    
 
 
-// Using templating engines pug, handlebars, ejs
-// In order to make express aware of using templating engines we must set a global configuration object in an express application using app.set(). These can be keys or configuration objects which express does not understand & ignores them but we can read them by accessing using app.get(). This can be other way of sharing data across express application
-// (views, view engine, etc) are some of reserved global configuration objects provided by express.
-// view engine - represents the registered templating engine that express has to use to deliver dynamic content templates [eg:- app.set('view engine', 'pug')].
-// views - tells express where to find these dynamic views. The default value for views is views folder in root directory but if we want to re-configure to some other folder value needs to be explicitly set.
-// We have added pug files in views(shop.pug)
-// router.get('/', (req, res, next) => {     // express will render 'shop.pug' file in views with configured templating engine extension(pug) for this route.
-//     res.render('shop');    
-// });
-// if prods.length > 0      // if statement in pug
-// each product in prods     // for each loop in pug
-// h1.product__title #{product.title}    // output dynamic content
+// block styles - block keyword in pug allows to add extra html lines in other files (eg:- additional css files linked with link tags for which 'styles' is placeholder).
+//extends layouts/main-layout.pug   // extends the common layout into other pages(eg:- header).
+// block content            // defining what the content variable must render in 404 page.
+//      h1 Page not found
 
 
