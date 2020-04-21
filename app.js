50dab445cb40df9a14ebeb9f0cd846abb5d873d0
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-21 17:29:01 +0530

    Serving static files using express.static() eg <link rel="stylesheet" href="/css/product.css">  To access these static files, 1) express.js hands over these routes to file system & for express to recognize, we use app.use(express.static(path.join(__dirname, 'public'))); 2) static method serves all static files(css, js, images, etc) in public directory. 3) Multiple static folders can be declared like public using above method. 4) The path for a static file route can start from root of the static folder, eg:- For a static folder, public/css/main.css, the css file can be accessed using <link rel="stylesheet" href="/css/main.css">

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/admin', adminRoutes);
 
 app.use(shopRoutes);
