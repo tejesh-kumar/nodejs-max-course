9824cd597d4b587e4462f3b5e79529164feb1a8d
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-24 10:01:56 +0530

    Storing & retreiving data into file using model saving products to a file instead of pushing to array. data folder created in main directory with products.json file in it. fs.readFile() - reads the complete file fs.createReadStream - Reading data in chunks & start working on them if it is big file.
    fs.readFile(p, (err, fileContent) => {
        // console.log(err);
        let products = [];                         // reading data from file for previously saved products.
        if(!err) {                                 // if(err) => 'no such file or directory', means no products previously saved as there is no file, hence products = [].
            products = JSON.parse(fileContent);    // if(!err) => parse & read content of file.
        }
        products.push(this);
        fs.writeFile(p, JSON.stringify(products), (err) => {
            console.log(err);
        });
    })
    
    fs.readFile(p, (err, fileContent) => {
        if(err) {                                // err - No file in this name, hence no products
            return [];                           // reading data from file
        }
        return(JSON.parse(fileContent));
    })
    
    static fetchAll() {
        const p = path.join(                            // getting an error in products.js as products.length is not defined, due to asynchronous nature of code, products.length is executed before products array is returned from product.js model.
            path.dirname(process.mainModule.filename),
            'data',
            'products.json'
        );
    
        fs.readFile(p, (err, fileContent) => {
            if(err) {
                return [];
            }
            return(JSON.parse(fileContent));
        })
    }

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
     constructor(t) {
@@ -6,10 +7,38 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);      // 'this' represents the object created by above constructor function.
+        const p = path.join(
+            path.dirname(process.mainModule.filename), 
+            'data', 
+            'products.json'
+        );                              
+
+        fs.readFile(p, (err, fileContent) => {
+            // console.log(err);
+            let products = [];
+            if(!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+            
+        })                   
     }
 
-    static fetchAll() {         // static method allows to call the function on class itself instead of instantiated object.
-        return products;
+    static fetchAll() {   
+        const p = path.join(
+            path.dirname(process.mainModule.filename), 
+            'data', 
+            'products.json'
+        );  
+        
+        fs.readFile(p, (err, fileContent) => {
+            if(err) {                                // err - No file in this name, hence no products
+                return [];
+            }
+            return(JSON.parse(fileContent));
+        })
     }
 }
