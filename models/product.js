8e798f1170dba9fe089477106434e37c86bc613f
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-25 15:08:44 +0530

    Refactoring product.js for code re-usability const p = path.join(                                // These 2 helper functions for re-usability in save() & fetchAll() methods.     path.dirname(process.mainModule.filename),     'data',     'products.json' );
    const getContentsFromFile = (cb) => {
        fs.readFile(p, (err, fileContent) => {
            if(err) {                                // cb - callback function is passed to fetchAll(), hence once it returns the array to products, then it is res.render({...}) rendered.
               cb([]);
            }
            else {
                cb(JSON.parse(fileContent));
            }
        })
    }

--- models/product.js
+++ models/product.js
@@ -1,44 +1,38 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+    path.dirname(process.mainModule.filename), 
+    'data', 
+    'products.json'
+);  
+
+const getContentsFromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if(err) {                                // cb - callback function is passed to fetchAll(), hence once it returns the array to products, then it is res.render({...}) rendered.
+           cb([]);
+        }
+        else {
+            cb(JSON.parse(fileContent)); 
+        }
+    })
+}
+
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
 
     save() {
-        const p = path.join(
-            path.dirname(process.mainModule.filename), 
-            'data', 
-            'products.json'
-        );                              
-
-        fs.readFile(p, (err, fileContent) => {
-            // console.log(err);
-            let products = [];
-            if(!err) {
-                products = JSON.parse(fileContent);
-            }
+        getContentsFromFile((products) => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
-            
-        })                   
+        });
     }
 
     static fetchAll(cb) {   
-        const p = path.join(
-            path.dirname(process.mainModule.filename), 
-            'data', 
-            'products.json'
-        );  
-        
-        fs.readFile(p, (err, fileContent) => {
-            if(err) {                                // cb - callback function is passed to fetchAll(), hence once it returns the array to products, then it is res.render({...}) rendered.
-                cb([]);
-            }
-            cb(JSON.parse(fileContent)); 
-        })
+        getContentsFromFile(cb);
     }
 }
