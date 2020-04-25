4201701bd96cd78af799e2e5f11581d8d652afd5
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-26 00:17:11 +0530

    Extracting dynamic params from url router.get('/products/:productId', shopController.getProducts); :productId - productId can be any dynamic content(: - tells that the part of route can be anything). This dynamic value can be extracted from url using that name 'productId'.
    In this case,
    router.get('/products/delete', shopController.delete);
    router.get('/products/:productId', shopController.getProducts);
    order of the static route must be above dynamic route. Hence, only that specific route can be handled by static route and other routes by dynmic route.
    
    To access the productId from url,
     const prodId = req.params.productId;

--- routes/shop.js
+++ routes/shop.js
@@ -10,6 +10,8 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+
 router.get('/cart', shopController.getCart);
 
 router.get('/orders', shopController.getOrders);
@@ -20,3 +22,14 @@ module.exports = router;
 
 
 
+// router.get('/products/:productId', shopController.getProducts);
+// :productId - productId can be any dynamic content(: - tells that the part of route can be anything).
+// This dynamic value can be extracted from url using that name 'productId'.
+
+// In this case, 
+// router.get('/products/delete', shopController.delete);
+// router.get('/products/:productId', shopController.getProducts);
+// order of the static route must be above dynamic route. Hence, only that specific route can be handled by static route and other routes by dynmic route.
+
+// To access the productId from url,
+//  const prodId = req.params.productId;
