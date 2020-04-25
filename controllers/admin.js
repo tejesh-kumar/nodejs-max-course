bdf8831645a6b64bd7596a5553010be599bf8877
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-25 22:21:26 +0530

    Storing & Displaying products. Storing the title, imgUrl, price, description. Displaying all the products.

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,9 +10,14 @@ exports.getAddProduct = (req, res, next) => {
     });
 }
 
-exports.postAddProduct = (req, res, next) => {             
-    const product = new Product(req.body.title);      // product is created by instantiation of class.
-    product.save();                                  // To save the product
+exports.postAddProduct = (req, res, next) => { 
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+
+    const product = new Product(title, imageUrl, description, price);      // product is created by instantiation of class.
+    product.save();                                                      // To save the product
     res.redirect('/');                   
 }
 
