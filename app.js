351568f8242ae40bd2537508605190c7e27d62df
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-28 14:12:46 +0530

    Fetching data from database using promises     const db = require('./util/database');
        db.execute('SELECT * FROM products')
        .then(result => {
            console.log(result[0], result[1]);
        })
        .catch(err => {
            console.log(err);
        });
    result[0] contains the fetched data.

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const db = require('./util/database');
 
+db.execute('SELECT * FROM products')
+.then(result => {
+    console.log(result[0], result[1]);
+})
+.catch(err => {
+    console.log(err);
+});
+
 const app = express();   
 
 app.set('view engine', 'ejs');      // ejs is a registered templating engine to deliver templates.
@@ -24,17 +32,3 @@ app.use(errorController.get404);
 app.listen(3000);    
 
 
-// created mysql database.
-// npm install --save mysql2   => 'mysql2' package installed to interact with mysql.
-// Create database.js in utils => code that connects to mysql database & give us back a connection object which allows to run queries.
-// A connection must be established to run queries and close the connection, doing manually is practically impossible for each query. Hence, we can create a connection pool. This pool contains connections which can be used for each query, the connections in pool get exhausted on shutting down the program.
-    // const pool = mysql.createPool({
-    //     host: 'localhost',
-    //     user: 'root',
-    //     database: 'node-complete',
-    //     password: 'tejesh123'
-    // });
-    // module.exports = pool.promise();
-// db - contains pool of connections to run queries.
-// db.execute('SELECT * FROM products') - used to execute queries.
-// db.end() - used when the application needs to shut down.
