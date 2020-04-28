1290ac7828c0376de9c5816bdebe7abe0765142d
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-28 17:05:42 +0530

    Connecting Sequelize to mysql database. Sequelize is an object relational mapping library. Sequelize requires 'mysql2' package. Steps to execute in sequelize :- 1. Create a model & connect to a database.
    In database.js file, code written to connect sequelize to the database,
        const Sequelize = require('sequelize');
        const sequelize = new Sequelize('node-complete', 'root', 'tejesh123', {
            dialect: 'mysql',
            host: 'localhost'
        });
        module.exports = sequelize;
    'sequelize' is an instance, which is also a connection pool.
    Since, we are importing a class or constructor function it is named as 'Sequelize'.

--- app.js
+++ app.js
@@ -24,4 +24,16 @@ app.use(errorController.get404);
 app.listen(3000);    
 
 
+// Sequelize is an object relational mapping library.
+// Sequelize requires 'mysql2' package.
+// Steps to execute in sequelize :-
+// 1. Create a model & connect to a database.
 
+// In database.js file, code written to connect sequelize to the database,
+// const Sequelize = require('sequelize');   // Since, we are importing a class or constructor function it is named as 'Sequelize'.
+// const sequelize = new Sequelize('node-complete', 'root', 'tejesh123', {
+//     dialect: 'mysql',
+//     host: 'localhost'
+// });
+// module.exports = sequelize;           
+// 'sequelize' is an instance, which is also a connection pool.
