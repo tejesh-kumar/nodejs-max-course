b0139be50972a41b334614d4fa32c845ef71e3a0
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-29 16:07:54 +0530

    Association(relations) across different models. Each model can have one-one, one-many, many-one relationships with other models. To represent these associations with Sequelize, In app.js, before syncing with database,
        Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
    User created the product, "onDelete: 'CASCADE'" implies if a user is deleted, his relative products should get deleted.
    
        User.hasMany(Product, {constraints: true});
    One user can add or create multiple products.
    
        sequelize.sync({force: true})
        .then(result => {
            // console.log(result);
            app.listen(3000);
        })
        .catch(err => console.log(err));
    force: true => any previously created tables with no relations can be forced to over-ride to follow relations or associations with other tables as configured using above model-relations. This parameter not used in production.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();   
 
@@ -21,7 +23,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product, {constraints: true});
+
+sequelize.sync({force: true})
 .then(result => {
     // console.log(result);
     app.listen(3000); 
@@ -29,4 +34,20 @@ sequelize.sync()
 .catch(err => console.log(err));
 
 
+// Association(relations) across different models.
+// Each model can have one-one, one-many, many-one relationships with other models. To represent these associations with Sequelize,
+// In app.js, before syncing with database,
 
+    // Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+// User created the product, "onDelete: 'CASCADE'" implies if a user is deleted, his relative products should get deleted.
+
+    // User.hasMany(Product, {constraints: true});
+// One user can add or create multiple products.
+
+    // sequelize.sync({force: true})
+    // .then(result => {
+    //     // console.log(result);
+    //     app.listen(3000); 
+    // })
+    // .catch(err => console.log(err));
+// force: true => any previously created tables with no relations can be forced to over-ride to follow relations or associations with other tables as configured using above model-relations. This parameter not used in production.
