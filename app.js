c31df2dd51ae066384dbc3b29ce9039ea7939e73
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-30 00:56:26 +0530

    One-Many & Many-Many relations

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 
 const app = express();   
 
@@ -19,7 +22,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(5)
+    User.findByPk(1)
     .then(user => {
         req.user = user;
         next();              // We stored the sequelize object 'user' in 'req' object, but, we are not sending response => we use next() so that the request is funneled to next middlewares.
@@ -33,11 +36,15 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product, {constraints: true});
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 // sequelize.sync({force: true})
 sequelize.sync()
 .then(result => {
-    return User.findByPk(5); 
+    return User.findByPk(1); 
 })
 .then((user) => {
     if(!user) {
