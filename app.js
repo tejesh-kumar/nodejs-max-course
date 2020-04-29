5f2163e2bc4c397bf552b36f6e67eed367f813e3
tejesh <tejeshmuralikrishna@gmail.com>
2020-04-29 17:02:38 +0530

    Creating a dummy user
        sequelize.sync()
        .then(result => {
            return User.findByPk(5);
        })
        .then((user) => {
            if(!user) {
                return User.create({ name: 'tejesh', email: 'tejesh@gmail.com' });
            }
            // return Promise.resolve('user');
            return user;
        })
        .then((user) => {
            // console.log(user);
            app.listen(3000);
        })
        .catch(err => console.log(err));
    Once all the tables are created, in then()
    If user exists, we return user-object but if user does not exist we return a user-promise, hence to return always a consistent user-promise, we used Promise.resolve(user);
    But, by default when we are returning any object into then(), it internally returns a promise-object that can be resolved in then(). Hence, we can return only a object, return user;
    
        app.use((req, res, next) => {
            User.findByPk(5)
            .then(user => {
                req.user = user;
                next();              // We stored the sequelize object 'user' in 'req' object, but, we are not sending response => we use next() so that the request is funneled to next middlewares.
            })
        });
    A middleware function executed to fetch the user on incoming requests.
    Initially, when server starts, it sets up tables & starts listening to incoming requests. The above middleware is just registered and is executed only on incoming requests. Hence, we are very much guaranteed that we get a user from database using above middleware on incoming requests.
     We want to store this user in a request, req.user = user; => Valid as we can add a new field to a existing request(req) object but not over-riding some of existing fields like 'body'.
    This user retrieved from db is not just a normal js object containing values but is a 'sequelize object' stored in the database => we can utilize all sequelize methods like (destroy, etc).
    Now, we can use the user to create other products.

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ app.set('views', 'views');        // views in root folder is the place to find d
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(5)
+    .then(user => {
+        req.user = user;
+        next();              // We stored the sequelize object 'user' in 'req' object, but, we are not sending response => we use next() so that the request is funneled to next middlewares.
+    })
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -26,28 +34,62 @@ app.use(errorController.get404);
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product, {constraints: true});
 
-sequelize.sync({force: true})
+// sequelize.sync({force: true})
+sequelize.sync()
 .then(result => {
-    // console.log(result);
+    return User.findByPk(5); 
+})
+.then((user) => {
+    if(!user) {
+        return User.create({ name: 'tejesh', email: 'tejesh@gmail.com' });
+    } 
+    // return Promise.resolve('user');
+    return user;
+})
+.then((user) => {
+    // console.log(user);
     app.listen(3000); 
 })
 .catch(err => console.log(err));
 
 
-// Association(relations) across different models.
-// Each model can have one-one, one-many, many-one relationships with other models. To represent these associations with Sequelize,
-// In app.js, before syncing with database,
+// Creating a dummy user
 
-    // Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
-// User created the product, "onDelete: 'CASCADE'" implies if a user is deleted, his relative products should get deleted.
-
-    // User.hasMany(Product, {constraints: true});
-// One user can add or create multiple products.
-
-    // sequelize.sync({force: true})
+    // sequelize.sync()
     // .then(result => {
-    //     // console.log(result);
+    //     return User.findByPk(5); 
+    // })
+    // .then((user) => {
+    //     if(!user) {
+    //         return User.create({ name: 'tejesh', email: 'tejesh@gmail.com' });
+    //     } 
+    //     // return Promise.resolve('user');
+    //     return user;
+    // })
+    // .then((user) => {
+    //     // console.log(user);
     //     app.listen(3000); 
     // })
     // .catch(err => console.log(err));
-// force: true => any previously created tables with no relations can be forced to over-ride to follow relations or associations with other tables as configured using above model-relations. This parameter not used in production.
+
+
+
+    // app.use((req, res, next) => {
+    //     User.findByPk(5);
+    // });
+// Once all the tables are created, in then()
+// If user exists, we return user-object but if user does not exist we return a user-promise, hence to return always a consistent user-promise, we used Promise.resolve(user);
+// But, by default when we are returning any object into then(), it internally returns a promise-object that can be resolved in then(). Hence, we can return only a object, return user;
+
+    // app.use((req, res, next) => {
+    //     User.findByPk(5)
+    //     .then(user => {
+    //         req.user = user;
+    //         next();              // We stored the sequelize object 'user' in 'req' object, but, we are not sending response => we use next() so that the request is funneled to next middlewares.
+    //     })
+    // });
+// A middleware function executed to fetch the user on incoming requests. 
+// Initially, when server starts, it sets up tables & starts listening to incoming requests. The above middleware is just registered and is executed only on incoming requests. Hence, we are very much guaranteed that we get a user from database using above middleware on incoming requests.
+//  We want to store this user in a request, req.user = user; => Valid as we can add a new field to a existing request(req) object but not over-riding some of existing fields like 'body'.
+// This user retrieved from db is not just a normal js object containing values but is a 'sequelize object' stored in the database => we can utilize all sequelize methods like (destroy, etc).
+// Now, we can use the user to create other products.
