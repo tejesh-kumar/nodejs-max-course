dc09d8ab318b9ff4c59d6442a83113d7edfab429
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-02 17:50:05 +0530

    Connecting to database     MongoClient.connect('mongodb+srv://tejesh:jeIJ3EMjPYgesXG8@max-node-vpc9g.mongodb.net/test?retryWrites=true&w=majority')     _db = client.db();     => client connecting to database mentioned in above connection string(test). If that db doesn't exist mongodb will create that db. Storing a connection to the database in '_db' variable.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database');
@@ -11,8 +11,8 @@ const mongoConnect = require('./util/database');
 
 const app = express();   
 
-app.set('view engine', 'ejs');      // ejs is a registered templating engine to deliver templates.
-app.set('views', 'views');        // views in root folder is the place to find dynamic views or templates is registered.
+app.set('view engine', 'ejs');     
+app.set('views', 'views');        
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,26 +25,15 @@ app.use((req, res, next) => {
     // })
 });
 
-// app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect((client) => {
-    console.log(client);
+mongoConnect(() => {
     app.listen(3000);
 });
 
 
 
 
-// Installing mongodb driver
-// npm install --save mongodb  => mongodb driver to connect to mongodb.
-
-    // const mongodb = require('mongodb');
-    // const MongoClient = mongodb.MongoClient;
-    // MongoClient.connect('mongodb+srv://tejesh:jeIJ3EMjPYgesXG8@max-node-vpc9g.mongodb.net/test?retryWrites=true&w=majority');
-// We can connect this 'MongoClient' to the mongodb database.
-// mongodb+srv://tejesh:<password>@max-node-vpc9g.mongodb.net/test?retryWrites=true&w=majority => connection-string. 
-// Replace the correct userName & password.
-// 'connect' method returns a promise
--- util/database.js
+++ util/database.js
@@ -1,14 +1,28 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+// '_' => this variable will be only used in this file
 
 const mongoConnect = (callback) => {
     MongoClient.connect('mongodb+srv://tejesh:jeIJ3EMjPYgesXG8@max-node-vpc9g.mongodb.net/test?retryWrites=true&w=majority')
     .then(client => {
         console.log('Connected');
+        _db = client.db();                // Storing an access to the database in _db
         callback(client);
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+        console.log(err);
+        throw err;
+    });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if(_db) {
+        return _db;
+    }
+    throw 'No database found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
