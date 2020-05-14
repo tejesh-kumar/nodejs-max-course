82d3e69dd3fb3c19a056efd93b518d69f53c6943
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-14 17:21:53 +0530

    Deleting Products In admin controller,     exports.postDeleteProduct = (req, res, next) => {         const prodId = req.body.productId;
            Product.findByIdAndRemove(prodId)
            .then((result) => {
                console.log('Product deleted successfully!');
                res.redirect('/admin/products');
            })
            .catch(err => console.log(err));
        }
    
    findByIdAndRemove() in mongoose helps to delete a product.

--- controllers/admin.js
+++ controllers/admin.js
@@ -81,7 +81,7 @@ exports.postEditProduct = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
 
-    Product.deleteById(prodId)
+    Product.findByIdAndRemove(prodId)
     .then((result) => {
         console.log('Product deleted successfully!');
         res.redirect('/admin/products');
